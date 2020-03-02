ccad3d91c224f757037e6ae572d2959122db8ed4
Frank Bruno <fbruno@asicsolutions.com>
2020-03-01 19:23:19 -0600

    Updated gitignore to allow .f files. Updated .f files for Altera libraries

---
+++ sim/files.f
@@ -0,0 +1,12 @@
++incdir+./
++libext+.sv+.v
+-v /edatools/intelFPGA_lite/./19.1/quartus/eda/sim_lib/arriaii_atoms.v
+-v /edatools/intelFPGA_lite/./19.1/quartus/eda/sim_lib/sgate.v
+-v /edatools/intelFPGA_lite/./19.1/quartus/eda/sim_lib/220model.v
+-v /edatools/intelFPGA_lite/./19.1/quartus/eda/sim_lib/altera_mf.v
+
+//-f ../src/tg68k/TG68K.f
+-F ../src/FX68K/fx68k.f
+-y ../src
+pll.sv
+../MacPlus.sv
---
+++ src/FX68K/fx68k.f
@@ -0,0 +1,3 @@
+fx68k.sv
+fx68kAlu.sv
+uaddrPla.sv
